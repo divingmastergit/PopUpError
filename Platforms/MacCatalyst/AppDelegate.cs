2758a095c5fb4b679e7cf476137b0a43b3643799
divingmastergit <86599249+divingmastergit@users.noreply.github.com>
2024-03-16 16:40:42 +0100

    Add files via upload

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace PopUPTest;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
