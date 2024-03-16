40540c34737e263ac27448b3507fa46a8cfd134f
divingmastergit <86599249+divingmastergit@users.noreply.github.com>
2024-03-16 16:39:22 +0100

    Initial Commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using CommunityToolkit.Maui.Views;
+
+namespace PopUPTest;
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnCounterClicked(object sender, EventArgs e)
+    {
+        var popup = new SimpleTestPopup();
+        await Application.Current.MainPage.ShowPopupAsync(popup);
+    }
+}
+
+public class SimpleTestPopup : Popup
+{
+    public SimpleTestPopup()
+    {
+            
+        var label = new Label
+        {
+            Text = "Test-Popup",
+            TextColor = Colors.Yellow,
+            IsVisible = true
+        };
+            
+        var frame = new Frame
+        {
+            Content = label,
+        };
+        Content = frame;
+
+    }
+}
